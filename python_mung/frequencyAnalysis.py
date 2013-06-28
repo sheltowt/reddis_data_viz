c3711c0e5482b7396dbd26f0c6331a7d83a1f448
Bill <sheltowt@gmail.com>
2013-06-28 14:19:24 -0700

    Frequency analysis for subreddits

--- python_mung/frequencyAnalysis.py
+++ python_mung/frequencyAnalysis.py
@@ -1,11 +1,28 @@
 import pandas as pd
+from collections import Counter
 
-data = pd.read_csv("/Users/williamshelton/Desktop/reddis_data_viz/data/prototype.csv")
+cnt = Counter()
 
-here = data["subreddit"]
+data = pd.read_csv("/Users/williamshelton/Desktop/reddis_data_viz2/data_by_subreddit/funny/funnyFiltered.csv")
 
-unique = here.value_counts()
+for sentence in data["words"]:
+  sentence = sentence.split()
+  for word in sentence:
+    cnt[word] +=1
 
-print unique
+print cnt
 
-unique.to_csv("/Users/williamshelton/Desktop/reddis_data_viz/data/unique_subreddit.csv")
+columns = ['word', 'frequency']
+index = range(0, 1400)
+
+
+freq = pd.DataFrame(index=index, columns=columns)
+
+count = 0
+for word in cnt:
+  if cnt[word] > 10:
+    freq["word"][count] = word
+    freq["frequency"][count] = cnt[word]
+    count = count + 1
+
+freq.to_json("/Users/williamshelton/Desktop/reddis_data_viz2/data_by_subreddit/funny/funnyFilteredFrequency.json")
--- python_mung/part_of_speech.py
+++ python_mung/part_of_speech.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import nltk
 
-subset = pd.read_csv("/Users/williamshelton/Desktop/reddis_data_viz/data_by_subreddit/funny/funny.csv")
+subset = pd.read_csv("/Users/williamshelton/Desktop/reddis_data_viz2/data_by_subreddit/funny/funny.csv")
 subset["words"] = ""
+
+desiredParts = ["NN", "NNP", "NNS", "NNPS", "VB", "VBD", "VBG", "VBP", "VBZ", "VBN", "JJ", "JJS", "JJR", "RB", "RBR", "RBS"]
 
 count = -1
 for title in subset["title"]:
@@ -14,8 +16,11 @@ for title in subset["title"]:
   clean.append(y)
   goodWords = ''
   for x in clean:
-  	if x[0][1] in ["NN", "NNP", "NNS", "NNPS", "VB", "VBD", "VBG", "VBP", "VBZ", "VBN", "JJ", "JJS", "JJR", "CC", "CD", "TO", "MD", "RB", "RBR", "RBS", "FW"]:
-  	  goodWords = goodWords + x[0][0]
-  subset["words"][count] = goodwords.lower()
+    for word in x:
+      print word
+      if word[0][1] in desiredParts:
+        print word[0][0]
+        goodWords = goodWords + " " + word[0][0]
+  subset["words"][count] = goodWords.lower()
 
-subset.to_csv("/Users/williamshelton/Desktop/reddis_data_viz/data_by_subreddit/funny/funnyFiltered.csv") 
+subset.to_csv("/Users/williamshelton/Desktop/reddis_data_viz2/data_by_subreddit/funny/funnyFiltered.csv") 
